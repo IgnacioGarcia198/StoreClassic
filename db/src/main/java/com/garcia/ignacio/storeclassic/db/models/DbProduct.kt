d6790f7cecaaf24613209943c492e0b24ac7fa0d
ignaciogarcia <ig@pep.security>
2023-08-03 09:08:20 +0200

    Add conversion to domain Product

--- db/src/main/java/com/garcia/ignacio/storeclassic/db/models/DbProduct.kt
+++ db/src/main/java/com/garcia/ignacio/storeclassic/db/models/DbProduct.kt
@@ -2,6 +2,7 @@ package com.garcia.ignacio.storeclassic.db.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.garcia.ignacio.domain.models.Product
 
 private const val PRODUCTS_TABLE_NAME = "products"
 
@@ -10,4 +11,8 @@ data class DbProduct(
     @PrimaryKey val code: String,
     val name: String,
     val price: Double,
-)
+) {
+    fun toDomain(): Product = Product(code, name, price)
+}
+
+fun Product.toDbProduct(): DbProduct = DbProduct(code, name, price)
