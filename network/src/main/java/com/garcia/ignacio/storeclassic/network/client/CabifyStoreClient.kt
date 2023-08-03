7e187122b37a880984a62c22a417f82413b5de8c
ignaciogarcia <ig@pep.security>
2023-08-03 09:33:40 +0200

    Extract StoreHttpClientFactory

--- network/src/main/java/com/garcia/ignacio/storeclassic/network/client/CabifyStoreClient.kt
+++ network/src/main/java/com/garcia/ignacio/storeclassic/network/client/CabifyStoreClient.kt
@@ -1,77 +1,26 @@
 package com.garcia.ignacio.storeclassic.network.client
 
-import android.util.Log
 import com.garcia.ignacio.storeclassic.data.remote.StoreClient
 import com.garcia.ignacio.storeclassic.domain.models.Product
 import com.garcia.ignacio.storeclassic.network.models.ProductsResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
-import io.ktor.client.engine.android.Android
-import io.ktor.client.plugins.DefaultRequest
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.observer.ResponseObserver
 import io.ktor.client.request.get
-import io.ktor.client.request.header
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
 private const val PRODUCTS_ENDPOINT =
     "https://gist.githubusercontent.com/palcalde/6c19259bd32dd6aafa327fa557859c2f/raw/ba51779474a150ee4367cda4f4ffacdcca479887/Products.json"
-private const val TIMEOUT = 60_000
 
-class CabifyStoreClient @Inject constructor(): StoreClient {
+class CabifyStoreClient @Inject constructor(
+    clientFactory: StoreHttpClientFactory
+) : StoreClient {
     private val httpClient: HttpClient by lazy {
-        createHttpClient()
+        clientFactory.createStoreHttpClient()
     }
 
     override suspend fun getProducts(): Flow<List<Product>> = flowOf(
         httpClient.get(PRODUCTS_ENDPOINT).body<ProductsResponse>().products.map { it.toDomain() }
     )
-
-    private fun createHttpClient() = HttpClient(Android) {
-        expectSuccess = true
-
-        install(ContentNegotiation) {
-            json(
-                Json {
-                    prettyPrint = true
-                    isLenient = true
-                    ignoreUnknownKeys = true
-                }
-            )
-
-            engine {
-                connectTimeout = TIMEOUT
-                socketTimeout = TIMEOUT
-            }
-        }
-
-        install(Logging) {
-            logger = object : Logger {
-                override fun log(message: String) {
-                    Log.v("Logger Ktor =>", message)
-                }
-
-            }
-            level = LogLevel.ALL
-        }
-
-        install(ResponseObserver) {
-            onResponse { response ->
-                Log.d("HTTP status:", "${response.status.value}")
-            }
-        }
-
-        install(DefaultRequest) {
-            header(HttpHeaders.ContentType, ContentType.Application.Json)
-        }
-    }
 }
