856b0d6ad974af610110f67962c9a6656ac25475
ignaciogarcia <ig@pep.security>
2023-08-05 07:52:04 +0200

    Add StoreViewModel

--- app/src/main/java/com/garcia/ignacio/storeclassic/FirstFragment.kt
+++ app/src/main/java/com/garcia/ignacio/storeclassic/FirstFragment.kt
@@ -1,10 +1,11 @@
 package com.garcia.ignacio.storeclassic
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.garcia.ignacio.storeclassic.databinding.FragmentFirstBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+    private val viewModel: StoreViewModel by viewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,8 +26,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
@@ -33,6 +34,7 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
---
+++ app/src/main/java/com/garcia/ignacio/storeclassic/StoreViewModel.kt
@@ -0,0 +1,24 @@
+package com.garcia.ignacio.storeclassic
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.garcia.ignacio.storeclassic.data.ProductsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StoreViewModel @Inject constructor(
+    private val repository: ProductsRepository
+) : ViewModel() {
+    init {
+        viewModelScope.launch {
+            repository.products.flowOn(Dispatchers.IO).collect {
+                Log.e("PRODUCTS", "PRODUCTS: $it")
+            }
+        }
+    }
+}
