627850800332e761b003510669748622128378dd
ignaciogarcia <ig@pep.security>
2023-08-06 22:07:43 +0200

    Make StoreClient @Singleton

--- network/src/main/java/com/garcia/ignacio/storeclassic/network/di/NetworkModule.kt
+++ network/src/main/java/com/garcia/ignacio/storeclassic/network/di/NetworkModule.kt
@@ -6,11 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Suppress("unused")
 @Module
 @InstallIn(SingletonComponent::class)
 interface NetworkModule {
     @Binds
+    @Singleton
     fun bindStoreClient(client: CabifyStoreClient): StoreClient
 }
