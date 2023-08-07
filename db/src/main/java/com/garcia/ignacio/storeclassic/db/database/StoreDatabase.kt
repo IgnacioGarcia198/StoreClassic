85497ecea6692fea7b7685d1b143f08cf4855698
ignaciogarcia <ig@pep.security>
2023-08-07 10:11:00 +0200

    Add type converter for list of Double

--- db/src/main/java/com/garcia/ignacio/storeclassic/db/database/StoreDatabase.kt
+++ db/src/main/java/com/garcia/ignacio/storeclassic/db/database/StoreDatabase.kt
@@ -2,6 +2,7 @@ package com.garcia.ignacio.storeclassic.db.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.garcia.ignacio.storeclassic.db.dao.DiscountDao
 import com.garcia.ignacio.storeclassic.db.dao.ProductDao
 import com.garcia.ignacio.storeclassic.db.models.DbDiscount
@@ -11,6 +12,7 @@ import com.garcia.ignacio.storeclassic.db.models.DbProduct
     entities = [DbProduct::class, DbDiscount::class],
     version = 1
 )
+@TypeConverters(com.garcia.ignacio.storeclassic.db.typeconverter.DbTypeConverters::class)
 abstract class StoreDatabase : RoomDatabase() {
     abstract val productDao: ProductDao
     abstract val discountDao: DiscountDao
---
+++ db/src/main/java/com/garcia/ignacio/storeclassic/db/typeconverter/DbTypeConverters.kt
@@ -0,0 +1,12 @@
+package com.garcia.ignacio.storeclassic.db.typeconverter
+
+import androidx.room.TypeConverter
+
+class DbTypeConverters {
+    @TypeConverter
+    fun doubleListToString(list: List<Double>) = list.joinToString(",")
+
+    @TypeConverter
+    fun stringToDoubleList(listString: String) =
+        listString.split(",").map { it.toDouble() }
+}
