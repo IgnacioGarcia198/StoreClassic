89124cd9f1aac729788c8ba1ab9918fb0ab11171
ignaciogarcia <ig@pep.security>
2023-08-09 08:05:43 +0200

    Simplify activity_main layout and remove fab

--- app/src/main/java/com/garcia/ignacio/storeclassic/ui/MainActivity.kt
+++ app/src/main/java/com/garcia/ignacio/storeclassic/ui/MainActivity.kt
@@ -3,7 +3,6 @@ package com.garcia.ignacio.storeclassic.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
@@ -32,12 +31,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
