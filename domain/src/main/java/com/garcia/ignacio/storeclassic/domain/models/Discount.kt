5043d2aae6d2e4a59f66f283b3397aaf4ad67669
ignaciogarcia <ig@pep.security>
2023-08-11 08:52:36 +0200

    Simplify logic in Discount
    Remove logic that is not being used at the moment.

--- domain/src/main/java/com/garcia/ignacio/storeclassic/domain/models/Discount.kt
+++ domain/src/main/java/com/garcia/ignacio/storeclassic/domain/models/Discount.kt
@@ -1,37 +1,17 @@
 package com.garcia.ignacio.storeclassic.domain.models
 
-private val ALL_PRODUCTS: String = ""
-
 sealed class Discount {
     abstract val productCode: String
 
-    fun apply(products: List<Product>): Double = when (productCode) {
-        ALL_PRODUCTS -> applyToAll(products)
-        else -> products.filter {
-            it.code == productCode
-        }.let { applicableProducts ->
-            if (applicableProducts.isNotEmpty()) applyDiscount(applicableProducts)
-            else 0.0
-        }
+    fun apply(applicableProducts: List<Product>): Double {
+        return if (applicableProducts.isEmpty()) 0.0 else applyDiscount(applicableProducts)
     }
 
     abstract fun partitionApplicableProducts(
         products: List<Product>
     ): Pair<List<Product>, List<Product>>
 
-
-    protected open fun applyToAll(products: List<Product>): Double {
-        return products.groupBy {
-            it.code
-        }.values.sumOf { group ->
-            if (group.isNotEmpty()) applyDiscount(group) else 0.0
-        }
-    }
-
     protected abstract fun applyDiscount(applicableProducts: List<Product>): Double
-
-    fun isApplicableTo(product: Product): Boolean =
-        productCode.isBlank() || productCode == product.code
 
     data class XForY(
         override val productCode: String,
@@ -40,10 +20,7 @@ sealed class Discount {
     ) : Discount() {
         override fun applyDiscount(applicableProducts: List<Product>): Double {
             val originalPrice = applicableProducts.first().price
-            val singleProducts = applicableProducts.size % productsBought
-            val completeGroupProducts = applicableProducts.size - singleProducts
-            return completeGroupProducts * originalPrice * productsPaid / productsBought +
-                    singleProducts * originalPrice
+            return applicableProducts.size * originalPrice * productsPaid / productsBought
         }
 
         override fun partitionApplicableProducts(
@@ -69,10 +46,7 @@ sealed class Discount {
     ) : Discount() {
         override fun applyDiscount(applicableProducts: List<Product>): Double {
             val originalPrice = applicableProducts.first().price
-            val price = if (applicableProducts.size >= minimumBought) {
-                originalPrice * (1 - discountPercent / 100)
-            } else originalPrice
-            return applicableProducts.size * price
+            return applicableProducts.size * originalPrice * (1 - discountPercent / 100)
         }
 
         override fun partitionApplicableProducts(
