5fe3de5cbdb75352e1cefeb78aa5477cd4c1f346
ignaciogarcia <ig@pep.security>
2023-08-13 16:57:01 +0200

    Add delay before updating data from network

--- network/src/main/java/com/garcia/ignacio/storeclassic/network/update/StoreDataUpdater.kt
+++ network/src/main/java/com/garcia/ignacio/storeclassic/network/update/StoreDataUpdater.kt
@@ -4,11 +4,14 @@ import com.garcia.ignacio.storeclassic.data.remote.ConnectivityMonitor
 import com.garcia.ignacio.storeclassic.data.repository.DiscountsRepository
 import com.garcia.ignacio.storeclassic.data.repository.ProductsRepository
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+private const val UPDATE_FROM_NETWORK_AFTER_CONNECTION_RESTORED_DELAY = 100L
 
 class StoreDataUpdater @Inject constructor(
     private val productsRepository: ProductsRepository,
@@ -27,14 +30,15 @@ class StoreDataUpdater @Inject constructor(
         connectivityMonitor.isNetworkConnectedFlow
             .onEach { isConnected ->
                 if (!wasConnected && isConnected) {
-                    updateDataFromNetwork()
+                    updateDataFromNetwork(UPDATE_FROM_NETWORK_AFTER_CONNECTION_RESTORED_DELAY)
                 }
                 wasConnected = isConnected
             }.launchIn(scope)
     }
 
-    private fun updateDataFromNetwork() {
+    private fun updateDataFromNetwork(delay: Long = 0L) {
         scope.launch {
+            delay(delay)
             updateDiscountsFromNetwork()
             updateProductsFromNetwork()
         }
